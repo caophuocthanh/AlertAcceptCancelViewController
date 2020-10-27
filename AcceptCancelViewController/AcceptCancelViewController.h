07210bc99b96341953d25dc04279027a468bd8fa
Cao Phuoc Thanh <caophuocthanh@gmail.com>
2020-10-28 01:02:52 +0700

    Initial Commit

---
+++ AcceptCancelViewController/AcceptCancelViewController.h
@@ -0,0 +1,19 @@
+//
+//  AcceptCancelViewController.h
+//  AcceptCancelViewController
+//
+//  Created by Cao Phuoc Thanh on 10/28/20.
+//  Copyright © 2020 Cao Phuoc Thanh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AcceptCancelViewController.
+FOUNDATION_EXPORT double AcceptCancelViewControllerVersionNumber;
+
+//! Project version string for AcceptCancelViewController.
+FOUNDATION_EXPORT const unsigned char AcceptCancelViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AcceptCancelViewController/PublicHeader.h>
+
+
